7d2c6b3d0f59ee66ef60e04987644db75c2b11e1
nasyatah <nasyatah@gmail.com>
2023-11-14 20:42:15 +0800

    updated code

--- spark-plumber1.R
+++ spark-plumber1.R
@@ -8,11 +8,14 @@ spark_model1 <- ml_load(sc, path = "spark_model1")
 
 #* @post /predict 
 
-function(Recency, Frequency, Monetary) {
+function(Recency, Frequency, Monetary, Average_basket_size, Unique_products, festive_spender) {
 new_data <- data.frame(
   Recency = as.numeric(Recency),
   Frequency = as.numeric(Frequency), 
   Monetary = as.numeric(Monetary),
+  Average_basket_size = as.numeric(Average_basket_size),
+  Unique_products = as.numeric(Unique_products),
+  festive_spender = festive_spender,
   logistic_duration = NA
 )
 new_data_r <- copy_to(sc, new_data, overwrite = TRUE)
